21201d28db3b89f46baf5c31f19e426abbe76a72
Mohamed <57219435+Poseidon1920@users.noreply.github.com>
2024-06-03 11:48:30 +0200

    updated

---
+++ assets/js/arrangementer.js
@@ -0,0 +1,8 @@
+import {getPostsByCategory} from "./app.js"
+import {RenderCards} from "./app.js"
+getPostsByCategory(11)
+.then(data => {
+    console.log(data)
+    RenderCards(data,false)
+})
+.catch(err => console.log(err))

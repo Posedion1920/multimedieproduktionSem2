120834955bdfc2e1ce6645a44322d904750e6968
Mohamed <57219435+Poseidon1920@users.noreply.github.com>
2024-06-10 13:33:48 +0200

    burger menu udvikling

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,36 @@
 const baseUrl = "https://0x.mohafh.dk/wp-json/wp/v2/posts";
 
 
+//burger menu
+
+const dropDowns = document.querySelectorAll(".fa-arrow-down");
+const dropDownElements1 = document.querySelector(".tilmeldDropdown");
+const dropDownElements2 = document.querySelector(".praktiskDropdown");
+
+const navBar = document.querySelector(".navBurger");
+
+dropDowns[0].addEventListener("click",function(e){
+    dropDownElements1.classList.toggle("show");
+})
+
+dropDowns[1].addEventListener("click",function(e){
+    dropDownElements2.classList.toggle("show");
+})
+
+
+const burgerMenuIkon = document.querySelector(".fa-bars");
+const nav = document.querySelector(".mobileNav");
+burgerMenuIkon.addEventListener("click",function(e){
+    navBar.classList.toggle("show")
+})
+
+
+
 // Main funktioner som skal bruges i andre js filer:
 
-
 // denne funktion fetcher posts som har et specifikt kategori id
+
+
 function getCategory(categoryId){
     return fetch(baseUrl+`?categories=${categoryId}&per_page=100`)
     .then(res => res.json())
@@ -413,9 +439,36 @@ function RenderTeam(post){
             continue
         }
     }
-
-    
 }
+
+function UpdateLabels(posts){
+    const youthTeam = document.querySelector(".ungdomsholdListe");
+    const boyTeamList = document.createElement("ul");
+    youthTeam.append(boyTeamList);
+    const boyParagraph = document.createElement("p");
+    boyParagraph.textContent = "Ungdom Drenge";
+    boyTeamList.append(boyParagraph);
+
+    posts.forEach(function(post){
+        if(post.categories.includes(14)){
+            const boyTeamLink = document.createElement("a");
+            boyTeamLink.href = `holdsiden.html?id=${post.id}`
+            boyTeamList.append(boyTeamLink);
+            const boyTeamLabel = document.createElement("li")
+            boyTeamLabel.textContent = post.acf.holdnavn;
+            boyTeamLink.append(boyTeamLabel);
+        }
+        else if(post.categories.includes(15)){
+            console.log(post.acf.holdnavn+"PIGER");
+        }
+        else if(post.categories.includes(17)){
+            console.log(post.acf.holdnavn+"DESMÅ")
+        }
+    })
+}
+
+getCategory(13)
+.then(data => UpdateLabels(data))
 
 
 

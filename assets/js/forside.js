2c285e639513d5fb0dd650081264444482eba6c0
Mohamed <57219435+Poseidon1920@users.noreply.github.com>
2024-06-11 14:47:45 +0200

    jp

--- assets/js/forside.js
+++ assets/js/forside.js
@@ -1,2 +1,25 @@
 getCategory(11)
 .then(data => renderEntryPoints(data))
+
+
+
+
+// function stopPlay(){
+//     if(video.paused){
+//         video.play()
+//     }
+//     else{
+//         video.pause()
+//         video.setAttribute("controls", "");
+//         if(video.hasAttribute("autoplay")){
+//             video.removeAttribute("autoplay");
+//         }
+//     }
+    
+// }
+
+
+// const video = document.querySelector(".herovideo");
+// video.addEventListener("click",function(e){
+//     stopPlay() 
+// })

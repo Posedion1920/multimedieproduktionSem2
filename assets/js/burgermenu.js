dc58eafa4fdfc9edea60abb76366847185fd6039
Mohamed <57219435+Poseidon1920@users.noreply.github.com>
2024-06-12 21:04:54 +0200

    wqeweq

--- assets/js/burgermenu.js
+++ assets/js/burgermenu.js
@@ -4,6 +4,7 @@ getCategory(13)
 const dropDowns = document.querySelectorAll(".fa-arrow-down");
 const dropDownElements1 = document.querySelector(".tilmeldDropdown");
 const dropDownElements2 = document.querySelector(".praktiskDropdown");
+const megaMenu = document.querySelector("#hideMegaMenu");
 
 const navBar = document.querySelector(".navBurger");
 
@@ -20,5 +21,11 @@ const burgerMenuIkon = document.querySelector(".fa-bars");
 const nav = document.querySelector(".mobileNav");
 burgerMenuIkon.addEventListener("click",function(e){
     navBar.classList.toggle("show")
+})
+
+
+dropDowns[2].addEventListener("click",function(e){
+    console.log(e);
+    megaMenu.classList.toggle("megaMenu");
 })
 
